4d40feb6df589b8901070151e4d24ba7957da12e
Marcin Florek <florekdeveloper@gmail.com>
2023-01-18 12:52:12 +0100

    correction

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.5235,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.6904,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.4279,
+    },
+];
