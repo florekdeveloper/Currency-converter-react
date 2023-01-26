6cb64af4d704a728609b5ca6f1178e02648a8b42
Marcin Florek <florekdeveloper@gmail.com>
2023-01-26 19:43:17 +0100

    styled-components added

--- src/App/styled.js
+++ src/App/styled.js
@@ -1,8 +1,10 @@
-.app {
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
     flex-basis: 400px;
     margin: 20px;
     padding: 40px;
     border-radius: 20px;
     box-shadow: 0 0 30px black;
     background-color: whitesmoke;
-}
+`;
