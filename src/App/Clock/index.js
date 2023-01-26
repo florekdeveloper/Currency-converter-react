e21d98adb5e34c0ac72ec161f1a99c4e5a490cba
Marcin Florek <florekdeveloper@gmail.com>
2023-01-26 19:36:59 +0100

    useCurrentDate added

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,32 +1,24 @@
-import React, { useState, useEffect } from "react";
 import { Wrapper } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
+
+const formatDate = (date) => date.toLocaleString("pl", {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+});
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, [] );
+    const date = useCurrentDate();
 
     return (
         <Wrapper>
             Dzisiaj jest
             {" "}
-            {date.toLocaleString("pl", {
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-            })}
+            {formatDate(date)}
         </Wrapper>
     )
 };
